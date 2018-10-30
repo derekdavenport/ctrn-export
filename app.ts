17b07fc9a04c3b4fdf2e911f753005e5dc65abd4
Derek Davenport <derekdavenport@gmail.com>
2018-10-30 15:49:24 -0400

    max file size and download retries

--- app.ts
+++ app.ts
@@ -5,11 +5,11 @@ import * as querystring from 'querystring';
 import { URL } from 'url';
 import { parse, HTMLElement } from 'node-html-parser';
 
-const [hostname, OrgID, UserId, pass] = process.argv.slice(2);
+const [hostname, OrgID, UserId, pass, noImageFilename, maxFileSizeMBvalue] = process.argv.slice(2);
 const baseURL = new URL('https://' + hostname + '/directory/');
-const noImageFilename = 'tn_b0539ee4ad44817af56d21786dd6b520_1362965845.jpg';
-
-const splitSize = 20 * 1024 * 1024; // bytes
+const maxFileSizeMB = parseInt(maxFileSizeMBvalue) || 15;
+const maxFileSize = maxFileSizeMB * 1024 * 1024;
+const maxRetries = 5;
 
 main();
 
@@ -26,26 +26,17 @@ async function main() {
 	try {
 		const directory = await loadDirectory();
 		const root = parse(directory);
-		let i = 0;
 		for (const userDiv of root.querySelectorAll('.group_user_div-tall') as HTMLElement[]) {
-			if (i++ < 4) continue;
 			const vcardLink = userDiv.querySelector('.vcard_image a') as HTMLElement;
 			const img = userDiv.querySelector('.newphotos_member') as HTMLElement;
 			const vcardURL = new URL(vcardLink.attributes['href'], baseURL);
 			const imageURL = new URL(img.attributes['src'], baseURL);
 			//const personId = vcardURL.searchParams.get('mid');
 			vcardPromises.push(makeVcard(vcardURL, imageURL));
-
-			// cut short while debugging
-			if (vcardPromises.length == 1)
-				break;
 		}
-		// TODO: split by splitSize
 		const vcards = await Promise.all(vcardPromises);
-		fs.writeFile('ctrn-vcards.vcf', vcards.join(''), error => {
-			console.log(error);
-			process.exit(1);
-		});
+		const fileNames = await writeVcards(vcards);
+		console.log('wrote ' + fileNames.join(', '));
 	}
 	catch (error) {
 		console.log(error);
@@ -53,25 +44,91 @@ async function main() {
 	}
 }
 
+async function writeVcards(vcards: string[]) {
+	return new Promise<string[]>(resolve => {
+		let i = 0;
+		let fileNames: string[] = [];
+		let stream: fs.WriteStream;
+		let writtenBytes = 0;
+		(function write() {
+			for (let ok = true; i < vcards.length && ok; i++) {
+				const vcardBuffer = Buffer.from(vcards[i]);
+				if (!stream || writtenBytes + vcardBuffer.byteLength > maxFileSize) {
+					if (stream) {
+						stream.end();
+					}
+					const fileName = `ctrn-vcards-${fileNames.length}.vcf`;
+					fileNames.push(fileName);
+					stream = fs.createWriteStream(fileName, { flags: 'w' });
+					writtenBytes = 0;
+				}
+				// Buffer.byteLength(str, 'utf8')
+				ok = stream.write(vcardBuffer);
+				writtenBytes += vcardBuffer.byteLength;
+			}
+			if (i < vcards.length) {
+				stream.once('drain', write);
+			}
+			else {
+				stream.end();
+				resolve(fileNames);
+			}
+		})();
+	});
+}
+
 async function makeVcard(vcardURL: URL, imageURL: URL) {
 	const hasImage = path.basename(imageURL.pathname) !== noImageFilename;
-	const [vcard, image] = await Promise.all([
-		get(vcardURL),
-		hasImage ? get(imageURL) : null
-	]);
+	const vcardPromise = get(vcardURL);
+	const imagePromise = hasImage ? get(imageURL) : null;
+	let vcard, image;
+	try {
+		image = await imagePromise;
+	}
+	catch (error) {
+		console.log('unable to download image for ' + vcardURL.searchParams.get('mid'));
+	}
+	try {
+		vcard = await vcardPromise;
+	}
+	catch (error) {
+		console.log('unable to download contact for ' + vcardURL.searchParams.get('mid'));
+		return '';
+	}
+
 	return vcard.toString()
 		// replace work line with image or remove
 		.replace(/^ADR;TYPE=work:.*\s*/m, image ? ('PHOTO;ENCODING=b;TYPE=JPEG:' + image.toString('base64')).replace(/(^.{75}|(?!^).{74}(?=.))/g, '$1\r\n ') + '\r\n' : '')
 		// change birthdays to ISO format (birthdays without a year will become 2001)
-		.replace(/^BDAY:(.*)/m, (_, dateValue) => 'BDAY:' + new Date(dateValue).toISOString().substring(0,10));
+		.replace(/^BDAY:(.*)/m, (_, dateValue) => {
+			try {
+				return 'BDAY:' + new Date(dateValue).toISOString().substring(0, 10);
+			}
+			catch (error) {
+				return 'BDAY;VALUE=text:' + dateValue;
+			}
+		});
 }
 
-async function get(url: URL) {
-	return new Promise<Buffer>(resolve => {
+async function get(url: URL, retries = 0) {
+	return new Promise<Buffer>((resolve, reject) => {
 		let buffers: Buffer[] = [];
 		const request = https.get(url, response => {
 			response.on('data', (chunk: Buffer) => buffers.push(chunk));
 			response.on('end', () => resolve(Buffer.concat(buffers)));
+		});
+		request.on('error', async error => {
+			let r = request;
+			// console.error(`${url} : ${e.message}`);
+			if (retries < maxRetries) {
+				try {
+					resolve(await get(url, retries + 1));
+				}
+				catch (retryError) {
+					reject(retryError);
+				}
+			}
+			reject(error);
 		});
 		request.end();
 	});
